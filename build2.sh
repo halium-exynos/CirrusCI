678ae1f125b55452c71ddca7c9e0fe0a8d57afef
Trần Nguyễn Tuấn Anh <43093213+dopaemon@users.noreply.github.com>
2021-09-10 23:42:08 +0700

    LineageOS

--- build2.sh
+++ build2.sh
@@ -27,6 +27,6 @@ make api-stubs-docs || echo no problem
 make system-api-stubs-docs || echo no problem
 make test-api-stubs-docs || echo no problem
 mka bacon -j$(nproc --all) &
-sleep 75m
+sleep 65m
 kill %1
 ccache -s
